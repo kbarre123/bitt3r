a3b17e93af5b5225f863902cbbe7507722477f78
KC Barrett <kbarre123@gmail.com>
2013-12-31 20:53:18 +0000

    Added About page & updated tests accordingly

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -17,4 +17,11 @@ describe "StaticPages" do
     end
   end
   
+  describe "About page" do
+    it "should have the content 'about'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('about')
+    end
+  end
+  
 end
