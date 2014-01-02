4b3216668c2c9b89beae661ffc540a6b9345388c
KC Barrett <kbarre123@gmail.com>
2014-01-02 22:47:11 +0000

    Updated w/ Bootstrap, simplified tests

--- Gemfile
+++ Gemfile
@@ -5,6 +5,9 @@ ruby '2.0.0'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.0.2'
 
+# Twitter Bootstrap with sass
+gem 'bootstrap-sass', '2.3.2.0'
+
 group :development, :test do
   # Use sqlite3 as the database for Active Record
   gem 'sqlite3', '1.3.8'
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,53 +2,34 @@ require 'spec_helper'
 
 describe "StaticPages" do
   
+  subject { page }
+  
   describe "Home page" do
-    it "should have the content 'bitt3r'" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      visit '/static_pages/home'
-      expect(page).to have_content('bitt3r')
-    end
-    it "should have the base title" do
-      visit '/static_pages/home'
-      expect(page).to have_title("bitt3r")
-    end
-    it "should not have a custom page title" do
-      visit '/static_pages/home'
-      expect(page).not_to have_title(" | home")
-    end
+    before { visit root_path }
+    
+    it { should have_content('bitt3r') }
+    #it { should have_title(full_title('')) }
+    it { should_not have_title(' | home') } 
   end
   
   describe "Help page" do
-    it "should have the content 'help'" do
-      visit '/static_pages/help'
-      expect(page).to have_content('help')
-    end
-    it "should have the title 'Help'" do
-      visit '/static_pages/help'
-      expect(page).to have_title("bitt3r | help")
-    end
+    before {visit help_path}
+   
+    it { should have_content('help') }
+    #it { should have_title(full_title('help')) }
   end
   
   describe "About page" do
-    it "should have the content 'about us'" do
-      visit '/static_pages/about'
-      expect(page).to have_content('about us')
-    end
-    it "should have the title 'about us'" do
-      visit '/static_pages/about'
-      expect(page).to have_title("bitt3r | about us")
-    end
+    before {visit about_path}
+   
+    it { should have_content('about') }
+    #it { should have_title(full_title('about')) }
   end
   
   describe "Contact page" do
-    it "should have the content 'contact us'" do
-      visit '/static_pages/contact'
-      expect(page).to have_content('contact us')
-    end
-    it "should have the title 'contact us'" do
-      visit '/static_pages/contact'
-      expect(page).to have_title("bitt3r | contact us")
-    end
+    before {visit contact_path}
+   
+    it { should have_content('contact') }
+    #it { should have_title(full_title('contact')) }
   end
-  
 end
