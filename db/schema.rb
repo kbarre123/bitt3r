575ce9f5b622a9199c9ea176ed7dc79dbc74c812
KC Barrett <kbarre123@gmail.com>
2014-01-08 19:52:34 +0000

    Restart Ch. 10

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140108024735) do
+ActiveRecord::Schema.define(version: 20140108193510) do
 
   create_table "users", force: true do |t|
     t.string   "name"
